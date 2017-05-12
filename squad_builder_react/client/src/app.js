d1eec24a2735b9a1aeea003ed8f50c4fc9245990
Ross Crichton <rcrichton87@googlemail.com>
2017-05-12 13:25:02 +0100

    modified app.js

--- squad_builder_react/client/src/app.js
+++ squad_builder_react/client/src/app.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/Home'
+import { HashRouter, Route, IndexRoute } from 'react-router-dom'
 
-window.onload = function(){
-  ReactDOM.render(
-    <h1> App Started </h1>,
-    document.getElementById('app')
-  );
+class App extends React.Component{
+
+  render(){
+    return (
+      <HashRouter>
+        <div className='container'>
+          <Route exact path="/" component={Home} />
+        </div>
+      </HashRouter>
+    )
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById('app'))
