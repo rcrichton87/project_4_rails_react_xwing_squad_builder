531bcd01dad4e88344fedb8685473ebee0cb7709
Ross Crichton <rcrichton87@googlemail.com>
2017-05-12 11:32:57 +0100

    api/squads route now displays squads for the current user, including the details of each piloted_ship, with each include the details of each pilot and ship

--- squad_builder_rails/app/controllers/squads_controller.rb
+++ squad_builder_rails/app/controllers/squads_controller.rb
@@ -2,15 +2,28 @@ class SquadsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    squads = current_user.squads
-    render :json => squads.to_json()
+    render json: Squad.where(user: current_user[:id]).as_json({
+      only: [:id, :name, :faction],
+      include: {
+        piloted_ships: {
+          only: [:id],
+          include: {
+            pilot: {
+              only: [:id, :name, :cost, :pilot_skill, :ability]
+              },
+            ship: {
+              only: [:id, :name, :cost, :faction, :stats, :upgrade_slots]
+            }
+          }
+        }
+      }
+      })
   end
 
   def create
     show = Squad.create( squad_params )
     render json: show, status: :created
   end
-
 
   private
   def squad_params
