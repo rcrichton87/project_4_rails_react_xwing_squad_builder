b32711278066fa26cce7e5d63654cd9dc38a378f
Ross Crichton <rcrichton87@googlemail.com>
2017-05-15 10:13:35 +0100

    clicking a squad name will expand it to show the ships in the squad

--- squad_builder_react/client/src/containers/SquadContainer.jsx
+++ squad_builder_react/client/src/containers/SquadContainer.jsx
@@ -5,26 +5,38 @@ class SquadContainer extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      squad: props.squad
+      squad: props.squad,
+      expanded: false
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(){
+    this.setState({expanded: !this.state.expanded})
   }
 
   render(){
-
-    const ships = this.state.squad.piloted_ships.map((pilotedShip, index) => {
+    if(this.state.expanded){
+      const ships = this.state.squad.piloted_ships.map((pilotedShip, index) => {
+        return(
+          <li key={index}>{pilotedShip.ship.name} - {pilotedShip.pilot.name}</li>
+        )
+      })
       return(
-        <li key={index}>{pilotedShip.ship.name} - {pilotedShip.pilot.name}</li>
+        <div onClick={this.handleClick} className="squad-container">
+          <p>{this.state.squad.name}</p>
+          <ul>
+            {ships}
+          </ul>
+        </div>
       )
-    })
-
-    return(
-      <div className="squad-container">
-        <p>{this.state.squad.name}</p>
-        <ul>
-          {ships}
-        </ul>
-      </div>
-    )
+    } else {
+        return(
+         <div onClick={this.handleClick} className="squad-container">
+          <p>{this.state.squad.name}</p>
+        </div>
+      )
+    }
   }
 }
 
